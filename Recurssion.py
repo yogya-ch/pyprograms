194679e005a1bec6fc2b93fecc22b2d7ea7c272a
yogya-ch <33741788+yogya-ch@users.noreply.github.com>
2018-01-31 21:32:37 +0530

    Add files via upload

---
+++ Recurssion.py
@@ -0,0 +1,29 @@
+#computing pow(a,b) using recurssion 
+'''def power(base,exp):
+	if(exp!=1):
+		return(base*power(base,exp-1))
+	else:
+		return base
+
+print(power(10,3))'''
+
+#computing factorial of a given number
+'''def factorial(n):
+	if(n!=1):
+		return(n*factorial(n-1))
+	else:
+		return 1
+
+print(factorial(4))'''
+
+#computing sum of a given list
+'''def sum_list(lis):
+	if len(lis)==0:
+        	return 0
+	else:
+	    	 return lis[0] + sum_list(lis[1:])
+print(sum_list([1,2,3,4]))'''
+
+
+
+
